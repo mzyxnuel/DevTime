ff4c9a88e15ec4dfaaaeccc2c66400e82ec66b40
mzyxnuel <137792640+mzyxnuel@users.noreply.github.com>
2024-03-19 18:11:41 +0100

    net: http handler

---
+++ app/src/main/java/timecode/model/HttpHandler.java
@@ -0,0 +1,47 @@
+package timecode.model;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class HttpHandler {
+   private DotEnv env = new DotEnv();
+   private HttpClient client = HttpClient.newHttpClient();
+
+   public HttpResponse<String> http(String method, String uri, String content) {
+      try {
+         String extension = "";
+         if (!env.envProd())
+            extension = ".php";
+
+         HttpRequest.Builder requestBuilder = HttpRequest.newBuilder()
+               .uri(new URI(
+                  env.get("DOMAIN")
+                  + uri
+                  + extension
+               ))
+               .header("Content-Type", "application/xml");
+
+         switch (method.toUpperCase()) {
+            case "GET":
+               requestBuilder.GET();
+               break;
+            case "POST":
+               requestBuilder.POST(HttpRequest.BodyPublishers.ofString(content));
+               break;
+            default:
+               throw new IllegalArgumentException("Invalid Method: " + method);
+         }
+
+         HttpRequest request = requestBuilder.build();
+         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+         return response;
+      } catch (IOException | InterruptedException | URISyntaxException e) {
+         System.out.println("[error]: connection failed");
+      }
+      return null;
+   }
+}
