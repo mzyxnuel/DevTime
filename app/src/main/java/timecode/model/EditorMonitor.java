daa7a1439783a6a5aa2b513bc57db1926e0f09d1
mzyxnuel <137792640+mzyxnuel@users.noreply.github.com>
2024-03-19 19:26:48 +0100

    logic: editor monitor

---
+++ app/src/main/java/timecode/model/EditorMonitor.java
@@ -0,0 +1,35 @@
+package timecode.model;
+
+import java.util.List;
+
+public class EditorMonitor implements Runnable {
+   private boolean isOpen = false;
+
+   @Override
+   public void run() {
+      String processName = "Code.exe";
+      while (true) {
+         List<ProcessHandle> processes = ProcessHandle.allProcesses().toList();
+         for (ProcessHandle process : processes) {
+            if (process.info().command().orElse("").contains(processName)) {
+               isOpen = true;
+               break;
+            } else
+               isOpen = false;
+         }
+
+         System.out.println("[monitor]: is editor open: " + isOpen);
+
+         try {
+            Thread.sleep(1000);
+         } catch (InterruptedException e) {
+            e.printStackTrace();
+         }
+      }
+   }
+
+   public boolean isOpen() {
+      return isOpen;
+   }
+
+}
