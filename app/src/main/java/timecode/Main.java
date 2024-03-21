afc3e0bb5f2bf7f67db64162a4093cb558e9aaa5
mzyxnuel <137792640+mzyxnuel@users.noreply.github.com>
2024-03-21 20:29:10 +0100

    enhance: editor monitor finally works!!

--- app/src/main/java/timecode/Main.java
+++ app/src/main/java/timecode/Main.java
@@ -1,30 +1,11 @@
 package timecode;
 
-import timecode.view.App;
+import timecode.model.local.EditorMonitor;
 
 public class Main {
    public static void main(String[] args) {
-      App.main(args);
+      // App.main(args);
 
-
-      // EditorMonitor monitor = new EditorMonitor();
-      // Thread t = new Thread(monitor, "ed");
-      // t.start();
-
-      // while (true) {
-      //    boolean isOpen = monitor.isOpen();
-      //    if (!isOpen) {
-      //       // http richiest + thread stop
-      //       System.out.println(System.getProperty("os.name"));
-      //       System.out.println(System.getProperty("user.dir"));
-      //       System.out.println(System.getProperty("user.name"));
-      //    }
-
-      //    try {
-      //       Thread.sleep(1000);
-      //    } catch (InterruptedException e) {
-      //       e.printStackTrace();
-      //    }
-      // }
+      new Thread(new EditorMonitor()).start();
    }
 }
--- app/src/main/java/timecode/model/local/EditorMonitor.java
+++ app/src/main/java/timecode/model/local/EditorMonitor.java
@@ -1,9 +1,18 @@
 package timecode.model.local;
 
+import java.time.Instant;
 import java.util.List;
 
+import timecode.model.Activity;
+import timecode.model.Activity.Files;
+import timecode.model.net.HttpHandler;
+import timecode.model.net.JAXB;
+
 public class EditorMonitor implements Runnable {
+   private long startTime;
+   private long endTime;
    private boolean isOpen = false;
+   private boolean actReg = false;
 
    @Override
    public void run() {
@@ -18,6 +27,26 @@ public class EditorMonitor implements Runnable {
                isOpen = false;
          }
 
+         if (!actReg && isOpen) { //if the edior is open for the first time start the activity
+            startTime = Instant.now().getEpochSecond();
+            actReg = true;
+         } else if (actReg && !isOpen) { // if the edior is closed and the activity is started the finish the activity
+            endTime = Instant.now().getEpochSecond();
+
+            System.out.println("[monitor] activity saved");
+            Activity activity = new Activity(getIdUser(), startTime, endTime, getProjectName(), getOs(), getFiles());
+
+            String xml = new JAXB(Activity.class).marshal(activity);
+
+            new HttpHandler().http(
+               "POST",
+               "/", //TODO
+               xml
+            );
+
+            actReg = false;
+         }
+
          System.out.println("[monitor]: is editor open: " + isOpen);
 
          try {
@@ -28,8 +57,32 @@ public class EditorMonitor implements Runnable {
       }
    }
 
+   private int getIdUser() {
+      return 0;
+   }
+
+   private String getProjectName() {
+      return "";
+   }
+
+   private String getOs() {
+      return System.getProperty("os.name");
+   }
+
+   private Files getFiles() {
+      Files files = new Files();
+      return files;
+   }
+
    public boolean isOpen() {
       return isOpen;
    }
 
+   public long getStartTime() {
+      return startTime;
+   }
+
+   public long getEndTime() {
+      return endTime;
+   }
 }
