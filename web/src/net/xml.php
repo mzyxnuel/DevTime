67285e72732096de2d644fca7dd0669c9f67fccf
TOSAT0 <micheletosato05@gmail.com>
2024-03-23 16:34:35 +0100

    فایلەکانی xml
    فەنکشنی دروستکردن بۆ دروستکردنی پەڕگەی xml ی وەڵام

--- web/src/net/xml.php
+++ web/src/net/xml.php
@@ -1,18 +1,19 @@
 <?php
-function req($xsd) {
-   $request = file_get_contents("php://input");
-   $xml = new DOMDocument();
-   $xml->loadXML($request);
+   function req($xsd) {
+      $request = file_get_contents("php://input");
+      $xml = new DOMDocument();
+      $xml->loadXML($request);
 
-   try {
-      if ($xml->schemaValidate($xsd))
-         return simplexml_load_string($request);
-   } catch (Exception $e) {
-      //TODO risposta xml che invalida la richiesta
+      try {
+         if ($xml->schemaValidate($xsd))
+            return simplexml_load_string($request);
+      } catch (Exception $e) {
+         //TODO risposta xml che invalida la richiesta
+      }
    }
-}
 
-function res($xml) {
-   header("Content-Type: application/xml; charset=utf-8");
-   echo $response = "<?xml version='1.0' encoding='UTF-8'?>" . $xml;
-}
+   function res($xml) {
+      header("Content-Type: application/xml; charset=utf-8");
+      echo $response = "<?xml version='1.0' encoding='UTF-8'?>" . $xml;
+   }
+?>
--- web/src/routes/auth/login.php
+++ web/src/routes/auth/login.php
@@ -1,10 +1,52 @@
 <?php
-include("C:/xampp/htdocs/TimeCode/web/src/net/xml.php"); //TODO se riesci a trovare una soluzione usando gli import relativi
+   include("C:/xampp/htdocs/TimeCode/web/src/net/xml.php"); //TODO se riesci a trovare una soluzione usando gli import relativi
 
-$content = req("C:/xampp/htdocs/TimeCode/web/src/net/xsd/login.xsd");
+   function createXML($id_user){
+      $filePath = 'login_response.xml'
+      $dom = new DOMDocument('1.0', 'utf-8');
 
-$email = $content->email;
+      $root = $dom->createElement('user');
+      $id = $dom->createElement('id_user', $id_user);
 
-//esempio risposta
-$xml = "<email>" . $email . "</email>";
-res($xml);
+      $root->appendChild($id);
+      $dom->appendChild($root);
+      $dom->save($filePath);
+   }
+
+   $user = "root";
+   $password = "";
+   $db = "timecode";
+   $host = "localhost";
+   $port = 3306;
+   $conn = new PDO("mysql:host=$host; dbname=$db; port=$port", $user, $password);
+
+   $content = req("C:\Users\MicheleTosato\Desktop\SCUOLA\QUINTA\TimeCode\web\src\net\xsd\login.xsd");
+   // $content = req("C:/xampp/htdocs/TimeCode/web/src/net/xsd/login.xsd");
+   $email = $content->email;
+   $password = $content->password;
+   $psw = password_hash($password, PASSWORD_BCRYPT);
+
+   echo "email: $email<br>";
+   echo "password: $password<br>";
+   echo "psw: $psw<br>";
+
+   //response
+   $xmlResponse = new DOMDocument();
+   $user = $xmlResponse->createElementNS()
+
+   try{
+      $res = $conn->query("SELECT * FROM users");
+
+
+
+      $xmlResponse->addChild()
+   }catch(EXception $e){
+      $
+   }
+
+   $xml = "<email>" . $email . "</email>";
+   res($xml);
+
+
+
+?>
