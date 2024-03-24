f4ccd3120dd8cfb6ff9227f2dfafad86d3537193
mzyxnuel <137792640+mzyxnuel@users.noreply.github.com>
2024-03-24 11:50:08 +0100

    fix: dynamic ui

--- app/src/main/java/timecode/Main.java
+++ app/src/main/java/timecode/Main.java
@@ -1,11 +1,9 @@
 package timecode;
 
-import timecode.model.local.EditorMonitor;
 import timecode.view.App;
 
 public class Main {
    public static void main(String[] args) {
-      new Thread(new EditorMonitor()).start();
       App.main(args);
    }
 }
