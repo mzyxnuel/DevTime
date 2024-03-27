2d8b1b518619919972871f8f6a6938d643a76319
mzyxnuel <137792640+mzyxnuel@users.noreply.github.com>
2024-03-27 22:36:31 +0100

    logic: added init dashboard controller

--- app/src/main/java/timecode/control/DashboardController.java
+++ app/src/main/java/timecode/control/DashboardController.java
@@ -1,5 +1,35 @@
 package timecode.control;
 
-public class DashboardController {
+import java.net.URL;
+import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.text.Text;
+
+public class DashboardController implements Initializable {
+   @FXML
+   private ComboBox<String> projectselector;
+   @FXML
+   private Text name;
+   @FXML
+   private Text time;
+
+   @Override
+   public void initialize(URL u, ResourceBundle r) {
+      String[] projects = {"p1","p2","p3"};
+      projectselector.getItems().addAll(projects);
+
+      //get req for all projects names
+      //get req for the author name
+
+		projectselector.setOnAction(this::updateHeader);
+   }
+
+   private void updateHeader(ActionEvent event) {
+      String selectedProject = projectselector.getValue();
+		name.setText(selectedProject);
+   }
 }
