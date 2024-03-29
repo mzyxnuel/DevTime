c764a095338dc59aa19f2044c676b1d22b151965
mzyxnuel <137792640+mzyxnuel@users.noreply.github.com>
2024-03-29 13:51:30 +0100

    enhance: dashboard response controller init

--- app/src/main/java/timecode/control/DashboardController.java
+++ app/src/main/java/timecode/control/DashboardController.java
@@ -1,6 +1,7 @@
 package timecode.control;
 
 import java.net.URL;
+import java.net.http.HttpResponse;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
@@ -18,9 +19,19 @@ import javafx.scene.chart.XYChart;
 import javafx.scene.control.ComboBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
+import timecode.model.local.DotEnv;
+import timecode.model.local.MessageManager;
+import timecode.model.net.HttpHandler;
+import timecode.model.net.JAXB;
+import timecode.model.responses.ResponseDashboard;
+import timecode.model.responses.ResponseDashboard.DatesContainer;
+import timecode.model.responses.ResponseDashboard.LanguagesContainer;
+import timecode.model.responses.ResponseDashboard.OssContainer;
+import timecode.model.responses.ResponseDashboard.ProjectNamesContainer;
 import timecode.view.components.SemiCircleChart;
 
 public class DashboardController implements Initializable {
+   HttpHandler http = new HttpHandler();
    @FXML
    private ComboBox<String> selector;
    @FXML
@@ -35,15 +46,47 @@ public class DashboardController implements Initializable {
    private Pane productivity;
 
    @Override
-   public void initialize(URL u, ResourceBundle r) {
-      initProjectSelector();
-      initLanguagesChart();
-      initOssChart();
-      initProductivityChart();
-      initTimeAreaChart();
+   public void initialize(URL u, ResourceBundle r) { make(null); }
+
+   private void make(String project) {
+      HttpResponse<String> response = http.http(
+         "GET",
+         "/activity?api_key=" + new DotEnv().getApiKey() + "&project=" + project,
+         null
+      );
+
+      if (response != null) {
+         ResponseDashboard res = (ResponseDashboard) new JAXB(ResponseDashboard.class).unmarshal(response.body());
+         String status = res.getState().substring(0, res.getState().indexOf("/"));
+
+         if (status.equals("success")) {
+            initProjectSelector(res.getProjectNamesContainer());
+            initLanguagesChart(res.getLanguagesContainer());
+            initOssChart(res.getOssContainer());
+            initProductivityChart(res.getIncrementalPercentage());
+            initTimeAreaChart(res.getDatesContainer());
+         } else
+            new MessageManager(res.getState());
+      }
    }
 
-   private void initTimeAreaChart() {
+   private void initProjectSelector(ProjectNamesContainer pnc) {
+      String[] projects = {"All"};
+      for (int i = 1; i < pnc.getProjectContainer().size(); i++)
+         projects[i] = pnc.getProjectContainer().get(i).getProjectName();
+
+      selector.getItems().addAll(projects);
+      selector.setOnAction(this::ui);
+   }
+
+   private void ui(ActionEvent event) {
+      if (!selector.getValue().equals("All"))
+         make(selector.getValue());
+      else
+         make(null);
+   }
+
+   private void initTimeAreaChart(DatesContainer datesContainer) {
       timechart.setTitle("Hours per day");
       NumberAxis x = new NumberAxis(1, 31, 1);
       x.setLabel("Day");
@@ -70,7 +113,7 @@ public class DashboardController implements Initializable {
       timechart.getData().addAll(ex, ex2);
    }
 
-   private void initProductivityChart() {
+   private void initProductivityChart(double d) {
       List<SemiCircleChart.Data> dataList = new ArrayList<>();
       dataList.add(new SemiCircleChart.Data(70));
 
@@ -78,7 +121,7 @@ public class DashboardController implements Initializable {
       productivity.getChildren().add(semicircleChart);
    }
 
-   private void initOssChart() {
+   private void initOssChart(OssContainer ossContainer) {
       oss.setTitle("Operating Systems");
       ObservableList<PieChart.Data> data = FXCollections.observableArrayList(
             new PieChart.Data("A", 10),
@@ -89,7 +132,7 @@ public class DashboardController implements Initializable {
       oss.setData(data);
    }
 
-   private void initLanguagesChart() {
+   private void initLanguagesChart(LanguagesContainer languagesContainer) {
       languages.setTitle("Languages");
       ObservableList<PieChart.Data> data = FXCollections.observableArrayList(
             new PieChart.Data("A", 10),
@@ -98,16 +141,5 @@ public class DashboardController implements Initializable {
          );
 
       languages.setData(data);
-   }
-
-   private void initProjectSelector() {
-      String[] projects = {"p1","p2","p3"};
-      selector.getItems().addAll(projects);
-      selector.setOnAction(this::updateHeader);
-   }
-
-   private void updateHeader(ActionEvent event) {
-      String selectedProject = selector.getValue();
-		name.setText(selectedProject);
    }
 }
